ce29b7830ec95e7ac7a621fd6d7b87bdbd747f52
王良 <841369634@qq.com>
2021-06-28 02:14:36 +0800

    new-sample: 添加@Cache304的样例。

---
+++ easyj-web-samples/easyj-web-cache304-sample/src/main/java/org/easyj/samples/web/cache304/SamplesCache304Application.java
@@ -0,0 +1,17 @@
+package org.easyj.samples.web.cache304;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 使用 {@link org.easyj.web.cache304.annotation.Cache304} 的样例
+ *
+ * @author wangliang181230
+ */
+@SpringBootApplication
+public class SamplesCache304Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamplesCache304Application.class, args);
+	}
+}
