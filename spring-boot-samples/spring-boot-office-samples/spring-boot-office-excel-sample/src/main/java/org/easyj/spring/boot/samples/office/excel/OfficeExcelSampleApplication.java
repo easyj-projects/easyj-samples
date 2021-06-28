bc3e2c5bf09a2bda22cb87a55aceca9d2923521c
王良 <841369634@qq.com>
2021-06-28 19:32:39 +0800

    optimize: 引用easyj-spring-boot，并调整样例。

---
+++ spring-boot-samples/spring-boot-office-samples/spring-boot-office-excel-sample/src/main/java/org/easyj/spring/boot/samples/office/excel/OfficeExcelSampleApplication.java
@@ -0,0 +1,19 @@
+package org.easyj.spring.boot.samples.office.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 使用 EasyJ的Excel相关功能 的样例
+ *
+ * @author wangliang181230
+ * @see org.easyj.web.office.excel.ExcelExport
+ * @see org.easyj.office.excel.util.ExcelUtils
+ */
+@SpringBootApplication
+public class OfficeExcelSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeExcelSampleApplication.class, args);
+	}
+}
---
+++ spring-boot-samples/spring-boot-office-samples/spring-boot-office-excel-sample/src/main/java/org/easyj/spring/boot/samples/office/excel/mockquery/QueryParam.java
@@ -0,0 +1,19 @@
+package org.easyj.spring.boot.samples.office.excel.mockquery;
+
+/**
+ * 模拟查询用的参数
+ *
+ * @author wangliang181230
+ */
+public class QueryParam {
+
+	private Integer dataCount;
+
+	public Integer getDataCount() {
+		return dataCount;
+	}
+
+	public void setDataCount(Integer dataCount) {
+		this.dataCount = dataCount;
+	}
+}
