b9a3fb9f34043d1318c3dd9d8f5011c9c68f94cc
王良 <841369634@qq.com>
2021-07-01 12:05:05 +0800

    optimize: 1）`org.easyj`改为`icu.easyj`；2）build升级为1.0.2版本，并优化pom配置。

---
+++ spring-boot-samples/spring-boot-poi-samples/spring-boot-poi-excel-afterturn-sample/src/main/java/icu/easyj/spring/boot/samples/poi/excel/restcontroller/TestExcelExportController.java
@@ -0,0 +1,33 @@
+package icu.easyj.spring.boot.samples.poi.excel.restcontroller;
+
+import java.util.List;
+
+import icu.easyj.spring.boot.samples.poi.excel.mockquery.MyEntity;
+import icu.easyj.spring.boot.samples.poi.excel.mockquery.MyEntityStorage;
+import icu.easyj.spring.boot.samples.poi.excel.mockquery.QueryParam;
+import icu.easyj.web.poi.excel.ExcelExport;
+import icu.easyj.web.util.HttpUtils;
+import io.swagger.annotations.ApiImplicitParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试 @ExcelExport 的 Controller
+ *
+ * @author wangliang181230
+ */
+@RestController
+public class TestExcelExportController {
+
+	@ExcelExport(fileNamePre = "测试Excel导出功能", dataType = MyEntity.class)
+	@ApiImplicitParam(name = "doExport", dataType = "Boolean", dataTypeClass = Boolean.class, defaultValue = "false", paramType = "query")
+	@GetMapping("/test/excel-export")
+	public List<MyEntity> testExcelExport(QueryParam param) {
+		// 重要：当此次请求为excel导出请求时，将分布参数清除
+		if (HttpUtils.isDoExportRequest()) {
+			param.setPageSize(0); // 设置为0，表示不分页
+		}
+
+		return MyEntityStorage.doQuery(param);
+	}
+}
