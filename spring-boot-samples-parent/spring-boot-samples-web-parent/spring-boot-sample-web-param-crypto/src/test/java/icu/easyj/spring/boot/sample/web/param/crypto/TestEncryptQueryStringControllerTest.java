14321fc54c9ff37e1fdcc2221ecd08f6c99ecd70
王良 <841369634@qq.com>
2021-07-17 01:24:57 +0800

    test: 参数加密解密的测试类，调整。

--- spring-boot-samples-parent/spring-boot-samples-web-parent/spring-boot-sample-web-param-crypto/src/test/java/icu/easyj/spring/boot/sample/web/param/crypto/TestEncryptQueryStringControllerTest.java
+++ spring-boot-samples-parent/spring-boot-samples-web-parent/spring-boot-sample-web-param-crypto/src/test/java/icu/easyj/spring/boot/sample/web/param/crypto/TestEncryptQueryStringControllerTest.java
@@ -7,6 +7,8 @@ import icu.easyj.web.param.crypto.IParamCryptoFilterProperties;
 import icu.easyj.web.param.crypto.IParamCryptoHandler;
 import icu.easyj.web.param.crypto.IParamCryptoHandlerProperties;
 import icu.easyj.web.param.crypto.ParamCryptoFilter;
+import icu.easyj.web.param.crypto.exception.ParamDecryptException;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,9 +54,7 @@ class TestEncryptQueryStringControllerTest {
 	}
 
 	/**
-	 * 测试正常的情况
-	 *
-	 * @throws Exception
+	 * case: 入参正常加密的情况
 	 */
 	@Test
 	void testEncryptQueryStringSuccess() throws Exception {
@@ -64,8 +64,8 @@ class TestEncryptQueryStringControllerTest {
 
 		// 创建模拟请求
 		MockHttpServletRequestBuilder mockRequest;
-		if (StringUtils.hasLength(cryptoFilterProperties.getParameterName())) {
-			mockRequest = MockMvcRequestBuilders.get(path).queryParam(cryptoFilterProperties.getParameterName(), encryptedQueryString);
+		if (StringUtils.hasLength(cryptoFilterProperties.getQueryStringName())) {
+			mockRequest = MockMvcRequestBuilders.get(path).queryParam(cryptoFilterProperties.getQueryStringName(), encryptedQueryString);
 		} else {
 			mockRequest = MockMvcRequestBuilders.get(path + "?" + encryptedQueryString);
 		}
@@ -85,33 +85,28 @@ class TestEncryptQueryStringControllerTest {
 	}
 
 	/**
-	 * 未加密
+	 * case: 入参未加密的情况
 	 */
 	@Test
 	void testEncryptQueryStringFail1() throws Exception {
 		String path = "/test/querystring";
 		String queryString = "s1=111啊啊啊&s2=222呀呀呀&s2=哇哇哇";
-		String encryptedQueryString = SYMMETRIC_CRYPTO.encryptBase64(queryString);
 
 		// 创建模拟请求
 		MockHttpServletRequestBuilder mockRequest;
-		if (StringUtils.hasLength(cryptoFilterProperties.getParameterName())) {
-			mockRequest = MockMvcRequestBuilders.get(path).queryParam(cryptoFilterProperties.getParameterName(), encryptedQueryString);
+		if (StringUtils.hasLength(cryptoFilterProperties.getQueryStringName())) {
+			mockRequest = MockMvcRequestBuilders.get(path).queryParam(cryptoFilterProperties.getQueryStringName(), queryString);
 		} else {
-			mockRequest = MockMvcRequestBuilders.get(path + "?" + encryptedQueryString);
+			mockRequest = MockMvcRequestBuilders.get(path + "?" + queryString);
 		}
 
 		// 发送请求
-		ResultActions resultActions = mockMvc.perform(mockRequest);
-		// 接收响应
-		MvcResult result = resultActions.andReturn();
-		// 获取响应
-		int status = result.getResponse().getStatus();
-//		String contentType = result.getResponse().getContentType();
-//		int contentLength = result.getResponse().getContentLength();
-		String content = result.getResponse().getContentAsString(StandardCharsets.UTF_8);
-		// 校验响应内容
-		assertEquals(HttpStatus.OK.value(), status);
-		assertEquals("{\"name\":\"111啊啊啊222呀呀呀,哇哇哇\"}", content);
+		try {
+			mockMvc.perform(mockRequest);
+			throw new RuntimeException("未抛出异常，和预期不一样，应该会抛出ParamDecryptException才对。");
+		} catch (RuntimeException e) {
+			Assertions.assertEquals(ParamDecryptException.class, e.getClass());
+			Assertions.assertEquals("入参未加密或格式有误，解密失败", e.getMessage());
+		}
 	}
 }
