f5d607596e086118fd249b20e45434b0e62c77fb
王良 <841369634@qq.com>
2021-08-03 15:40:30 +0800

    test: 添加 “基于Afterturn的Excel文件导入导出功能” 的测试用例。

---
+++ spring-boot-samples-parent/spring-boot-samples-poi-parent/spring-boot-sample-poi-excel-afterturn/src/test/java/icu/easyj/spring/boot/sample/poi/excel/restcontroller/ExcelExportControllerTest.java
@@ -0,0 +1,44 @@
+package icu.easyj.spring.boot.sample.poi.excel.restcontroller;
+
+import icu.easyj.spring.boot.test.BaseSpringBootMockMvcTest;
+import icu.easyj.web.constant.HttpHeaderConstants;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
+/**
+ * {@link ExcelExportController} 测试用例
+ *
+ * @author wangliang181230
+ */
+@SpringBootTest
+class ExcelExportControllerTest extends BaseSpringBootMockMvcTest {
+
+	@Test
+	void testExcelExport_NotDoExport() throws Exception {
+		super.mockGet("/test/excel-export/list")
+				.queryParam("nameLike", "bbb")
+				.queryParam("pageSize", "1")
+				.send()
+				.isOk()
+				.contentType().is(MediaType.APPLICATION_JSON)
+				.header().notContains(HttpHeaders.CONTENT_DISPOSITION).end()
+				.content().is(1)
+				.is("[{\"name\":\"aaabbb\",\"age\":4,\"birthday\":\"2016-12-31T16:00:00.000+00:00\",\"desc\":\"aaaaa\"}]");
+	}
+
+	@Test
+	void testExcelExport_DoExport() throws Exception {
+		super.mockGet("/test/excel-export/list")
+				.queryParam("doExport", "true")
+				.queryParam("nameLike", "bbb")
+				.queryParam("pageSize", "1")
+				.send()
+				.isOk()
+				.contentType().is("application/vnd.ms-excel")
+				.header()
+				.isMatch(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"*\"")
+				.is(HttpHeaders.CACHE_CONTROL, HttpHeaderConstants.NO_CACHE);
+	}
+}
