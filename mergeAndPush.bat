d4a0b91126e27bc3d05ba6af41d52abd71f44b99
王良 <841369634@qq.com>
2021-11-04 16:50:52 +0800

    新增mergeAndPush.bat

--- mergeAndPush.bat
+++ mergeAndPush.bat
@@ -3,13 +3,13 @@ git merge github/master
 git push origin --all
 
 git checkout github spring-boot-2.1.5.RELEASE
-git merge github/master
+git merge master
 
 git checkout github spring-boot-2.1.5.RELEASE
-git merge github/master
+git merge master
 
 git checkout github spring-boot-2.1.5.RELEASE
-git merge github/master
+git merge master
 
 git push github --all
 git push github --all
